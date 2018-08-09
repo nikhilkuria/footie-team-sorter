3a6691382a0709046bf4c5c75fbf0471a263f322
nikhilkuria <nikhilkuria@gmail.com>
2018-08-09 21:41:43 +0200

    input parametrised and added logs

--- team_builder.py
+++ team_builder.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import sys
 
 MASTER_LIST_FILE = 'store/player_master_list.csv'
 PLAYERS_LIST_FILE = 'player_list.csv'
@@ -11,6 +12,7 @@ TEAM_NAMES = [
 ]
 
 
+# Read the master list file and create a dict
 def build_master_list():
     master_list = {}
     with open(MASTER_LIST_FILE) as player_list:
@@ -22,6 +24,7 @@ def build_master_list():
     return master_list
 
 
+# Fetch the rating of players from master list and create a dict
 def build_ranked_players(players_list_file, master_list):
     current_list = {}
     with open(players_list_file) as player_list:
@@ -36,21 +39,30 @@ def build_ranked_players(players_list_file, master_list):
     return current_list
 
 
+# Pick random team names
 def get_random_team_name():
     prefix = random.choice(TEAM_NAMES[0])
     suffix = random.choice(TEAM_NAMES[1])
     return prefix+" "+suffix
 
-players_master_list = build_master_list()
-player_current_list = build_ranked_players(PLAYERS_LIST_FILE, players_master_list)
 
-sorted_players = sorted(player_current_list.items(), key=lambda kv: kv[1], reverse=True)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        players_list_file = sys.argv[1]
+    else:
+        players_list_file = PLAYERS_LIST_FILE
 
-for team_id in range(0, NUM_OF_TEAMS):
-    team = sorted_players[0+team_id::NUM_OF_TEAMS]
-    team_names = map(lambda player: player[0], team)
+    # Create a dict of players from the master list
+    players_master_list = build_master_list()
+    player_current_list = build_ranked_players(players_list_file, players_master_list)
 
-    print("*"+get_random_team_name()+"*")
-    print(*team_names, sep='\n')
-    print('\n')
+    sorted_players = sorted(player_current_list.items(), key=lambda kv: kv[1], reverse=True)
 
+    # Sort the players into teams
+    for team_id in range(0, NUM_OF_TEAMS):
+        team = sorted_players[0 + team_id::NUM_OF_TEAMS]
+        team_names = map(lambda player: player[0], team)
+
+        print("*" + get_random_team_name() + "*")
+        print(*team_names, sep='\n')
+        print('\n')
